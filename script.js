d73fdaa94d4c940dd875dbef106bd751bffc7c11
harshgit15 <harshgit15@gmail.com>
2026-06-27 13:28:31 +0530

    Changes made

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const accuracy = document.querySelector("#user-accuracy");
 let userLatitude;
 let userLongitude;
 let userAccuracy;
-
+let status = document.querySelector("#user-status");
 
 getLocationBtn.addEventListener("click", () => {
   getLocation();
@@ -14,6 +14,8 @@ getLocationBtn.addEventListener("click", () => {
 
 function getLocation(){
   if(navigator.geolocation){
+         status.textContent = "📍Getting your Location!";
+
     navigator.geolocation.getCurrentPosition(
 
      async function(position){
@@ -24,7 +26,10 @@ function getLocation(){
          longitude.textContent = userLongitude.toFixed(6);
 
          userAccuracy = position.coords.accuracy;
-         accuracy.textContent = `${userAccuracy} metres`;
+         accuracy.textContent = `${userAccuracy.toFixed(2)} metres`;
+
+          
+         status.textContent = "🌍 Finding your address";
 
         await  getAddress();
 
@@ -44,28 +49,34 @@ function getLocation(){
 
  
 async function getAddress(){
-  const response =await  fetch(
-    `https://nominatim.openstreetmap.org/reverse?lat=${userLatitude}&lon=${userLongitude}&format=json`,
-  );
+  try{
+     const response = await fetch(
+       `https://nominatim.openstreetmap.org/reverse?lat=${userLatitude}&lon=${userLongitude}&format=json`,
+     );
 
-  const data = await response.json();
+     const data = await response.json();
 
-  console.log(data);
+     console.log(data);
 
-  const userDistrict = data.address.state_district;
-  document.querySelector("#district").textContent = userDistrict;
+     const userDistrict = data.address.state_district;
+     document.querySelector("#district").textContent = userDistrict;
 
-  
-  const userCity = data.address.city;
-  document.querySelector("#city").textContent = userCity;
+     const userCity = data.address.city;
+     document.querySelector("#city").textContent = userCity;
 
-  const userState = data.address.state;
-  document.querySelector("#state").textContent = userState;
+     const userState = data.address.state;
+     document.querySelector("#state").textContent = userState;
 
-  const userCountry = data.address.country;
-  document.querySelector("#country").textContent = userCountry;
+     const userCountry = data.address.country;
+     document.querySelector("#country").textContent = userCountry;
 
-  const postalCode = data.address.postcode;
-  document.querySelector("#postal-code").textContent = postalCode;
+     const postalCode = data.address.postcode;
+     document.querySelector("#postal-code").textContent = postalCode;
 
+     status.textContent = "✅ Location found successfully!";
+  }catch(error){
+    alert(error.message);
+     status.textContent = "❌ Falied to fetch address!";
+
+  }
 }
